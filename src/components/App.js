3aff3a8114ec6e8aa2d382d07b132833a515e958
Kacper Tokaj <kacpertokajj@gmail.com>
2023-02-21 01:51:09 +0100

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import About from './About';
 
 function App() {
   return (
-    <></>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import '../styles/navbar.css'
+import { SlNote } from 'react-icons/sl'
+import { NavLink } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
+export default function Navbar() {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-header'>
+                    <div className='navbar-header-icon'>
+                        <SlNote />
+                    </div>
+                    <div className='navbar-header-title'>
+                        <NavLink to='/' className='navbar-header-title-content'>Good Notes</NavLink>
+                    </div>
+                </div>
+                <div className='navbar-mobile-menu-icon' onClick={handleClick}>
+                    {click ? <FaTimes /> : <FaBars />}
+                </div>
+                <ul className={click ? 'navbar-menu active' : 'navbar-menu'}>
+                    <li className='navbar-item'>
+                        <NavLink to='/login' className={({ isActive }) =>
+                            "navbar-link" + (isActive ? " activated" : "")
+                        }>Login</NavLink>
+                    </li>
+                    <li className='navbar-item' >
+                        <NavLink to='/register' className={({ isActive }) =>
+                            "navbar-link" + (isActive ? " activated" : "")
+                        }>Register</NavLink>
+                    </li>
+                    <li className='navbar-item'>
+                        <NavLink to='/about' className={({ isActive }) =>
+                            "navbar-link" + (isActive ? " activated" : "")
+                        }>About</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
