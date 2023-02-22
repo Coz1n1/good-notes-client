53379329b9b8ea9e6eca459bdfb0b3c46ba28428
Kacper Tokaj <kacpertokajj@gmail.com>
2023-02-22 13:24:32 +0100

    responsive form

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './Navbar'
 import '../styles/register.css'
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { AiOutlineRight } from 'react-icons/ai'
 
 export default function Register() {
 
@@ -17,13 +19,15 @@ export default function Register() {
         <>
             <Navbar />
             <div className='register-form'>
-                <div className='register-form-header'>Register</div>
+                <div className='register-form-header'>Create an account</div>
                 <div className='register-form-input-label'>Enter your username</div>
                 <input type='text' className='register-form-input' placeholder='Enter your username...' onChange={(e) => setUsername(e.target.value)}></input>
-                <div className='register-form-input-label'>Enter your password</div>
+                <div className='register-form-input-label'>Enter your password </div>
                 <input type='password' className='register-form-input' placeholder='Enter your password...' onChange={(e) => setPassword(e.target.value)}></input>
                 <button onClick={registerSubmit} className='register-form-submit'>Register</button>
+                <div className='register-form-password-reminder'>Have you forgotten your password?</div>
             </div>
+            <div className='register-to-login'>Already have an account?<NavLink to='/login' className='register-to-login-link'>Login<AiOutlineRight /></NavLink></div>
         </>
     )
 }
