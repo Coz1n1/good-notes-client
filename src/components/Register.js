88290e31510ef67896cd9acdfe4b12712131fca9
Kacper Tokaj <kacpertokajj@gmail.com>
2023-02-23 23:42:13 +0100

    reg request

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,15 +4,30 @@ import '../styles/register.css'
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { AiOutlineRight } from 'react-icons/ai'
+import Axios from 'axios'
 
 export default function Register() {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [com, setCom] = useState('')
 
     const registerSubmit = () => {
-        console.log(username + ' ' + password)
-
+        console.log(username + ' ' + password + ' ' + email)
+        if (username !== '' && password !== '' && email !== '') {
+            Axios.post('http://localhost:3002/register', {
+                username: username,
+                password: password,
+                email: email,
+            }).then((response) => {
+                console.log(response.data)
+                setCom(response.data.com)
+            })
+        } else {
+            setCom('fill in all blanks')
+            console.log('fill in all blanks')
+        }
     }
 
     return (
@@ -22,8 +37,11 @@ export default function Register() {
                 <div className='register-form-header'>Create an account</div>
                 <div className='register-form-input-label'>Enter your username</div>
                 <input type='text' className='register-form-input' placeholder='Enter your username...' onChange={(e) => setUsername(e.target.value)}></input>
-                <div className='register-form-input-label'>Enter your password </div>
+                <div className='register-form-input-label'>Enter your password</div>
                 <input type='password' className='register-form-input' placeholder='Enter your password...' onChange={(e) => setPassword(e.target.value)}></input>
+                <div className='register-form-input-label'>Enter your email</div>
+                <input type='text' className='register-form-input' placeholder='Enter your email...' onChange={(e) => setEmail(e.target.value)}></input>
+                <div className='register-form-feedback'>{com}</div>
                 <button onClick={registerSubmit} className='register-form-submit'>Register</button>
                 <div className='register-form-password-reminder'>Have you forgotten your password?</div>
             </div>
